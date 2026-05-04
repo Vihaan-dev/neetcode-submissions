0d321faeee8149341db269ab2e7a280b009e5567
Vihaan Mehrotra <vihaan.mehrotra.6@gmail.com>
2026-05-04 14:11:23 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        // A map where the Key is the sorted string, and Value is the group of anagrams
+        unordered_map<string, vector<string>> groups;
+        
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end()); // Anagrams will have the same sorted string
+            groups[key].push_back(s);     // O(1) average lookup and push
+        }
+        
+        vector<vector<string>> result;
+        for (auto& pair : groups) {
+            result.push_back(pair.second); // Move the groups into the final answer
+        }
+        
+        return result;
+    }
+};
