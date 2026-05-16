78d1690324debcd18a949f1732e95695f878f4b7
Vihaan Mehrotra <vihaan.mehrotra.6@gmail.com>
2026-05-16 13:53:29 +0530

    Add: rotate-array - submission-4

---
+++ Data Structures & Algorithms/rotate-array/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int>ans;
+        k%=nums.size();
+        for (int i=nums.size()-k;i<nums.size();i++){
+            ans.push_back(nums[i]);
+        } for (int i=0;i<nums.size()-k;i++){
+            ans.push_back(nums[i]);
+
+        }nums=ans;
+    }
+};
