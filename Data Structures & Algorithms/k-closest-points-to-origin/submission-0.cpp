03c8167719c9a1c53ed51a3c2de88922e92b9c4e
Vihaan Mehrotra <vihaan.mehrotra.6@gmail.com>
2026-06-09 19:43:10 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    struct ComparePairs {
+        bool operator()(vector<int> a,vector<int> b) const {
+        return (pow(a[0],2)+pow(a[1],2)<pow(b[0],2)+pow(b[1],2));
+        }
+    };
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>,vector<vector<int>>,ComparePairs> q;
+        for (auto el:points){
+            q.push(el);
+            if (q.size()>k) q.pop();
+
+        }
+        vector<vector<int>> ans;
+        for (int i=0;i<k;i++){
+            ans.push_back(q.top());
+            q.pop();
+        }
+        return ans;
+    }
+};
