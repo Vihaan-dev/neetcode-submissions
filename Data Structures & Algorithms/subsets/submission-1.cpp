638ab1ed7f6410bd5d29fb08ec62094e6f981e51
Vihaan Mehrotra <vihaan.mehrotra.6@gmail.com>
2026-06-11 20:33:01 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    
+public:
+    void recurse(vector<vector<int>>& ans, vector<int>& cur, span<int> l){
+        if (l.size()==0){
+            ans.push_back(cur);
+            return;
+        }
+        int el = l[0];
+        l=l.subspan(1);
+        recurse(ans,cur,l);
+        cur.push_back(el);
+        recurse(ans,cur,l);
+        cur.pop_back();
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> cur;
+        recurse(ans,cur,span<int>(nums));
+        return ans;
+
+
+    }
+};
